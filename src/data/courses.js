17642fd747b52ce0918f9a6e3cb56f19a318c8e9
Ishaan1007 <ishaanjain4u@gmail.com>
2026-02-06 23:25:40 +0530

    Update courses

--- src/data/courses.js
+++ src/data/courses.js
@@ -3,7 +3,7 @@ export const courses = [
     id: 1,
     title: 'C++ - Beginner to Advanced',
     category: 'C++',
-    level: 'Complete Project',
+    level: 'Beginner',
     thumbnail: '/images/thumbnails/cpp.jpg',
     youtubeId: 'https://youtu.be/e7sAf4SbS_g?si=4ntMC40OZiKTNtQ7',
     type: 'video'
@@ -12,7 +12,7 @@ export const courses = [
     id: 2,
     title: 'Python Full Course',
     category: 'Python',
-    level: 'Full Course',
+    level: 'One Shot',
     thumbnail: '/images/thumbnails/python.jpg',
     youtubeId: 'rfscVS0vtbw',
     type: 'video'
@@ -28,11 +28,56 @@ export const courses = [
   },
   {
     id: 4,
-    title: 'Full Web Dev Playlist',
-    category: 'Web Development',
+    title: 'C Programming - Full Course',
+    category: 'C Programming',
     level: 'Playlist',
     thumbnail: '/images/thumbnails/node.jpg',
     playlistId: 'PLxgZQoSe9cg1drBnejUaDD9GEJBGQ5hMt',
     type: 'playlist'
+  },
+  {
+    id: 5,
+    title: 'Data Structures and Algorithms in C++',
+    category: 'C++',
+    level: 'Intermediate',
+    thumbnail: '/images/thumbnails/dsa.jpg',
+    youtubeId: 'https://youtu.be/GRxzQXBwA-U?si=LaKrBPhAk2FHaZ8k',
+    type: 'video'
+  },
+  {
+    id: 6,
+    title: 'Bootstrap 5 Full Course',
+    category: 'Web Development',
+    level: 'Beginner',
+    thumbnail: '/images/thumbnails/bootstrap.jpg',
+    youtubeId: 'https://youtu.be/qNifU_aQRio?si=oDpkyIO_LG3nLWG4',
+    type: 'video'
+  },
+  {
+    id: 7,
+    title: 'Web Development full course',
+    category: 'Web Development',
+    level: 'Beginner',
+    thumbnail: '/images/thumbnails/webdev.jpg',
+    playlistId: 'PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w',
+    type: 'playlist'
+  },
+  {
+    id: 8,
+    title: 'Python Full Course for Beginners',
+    category: 'Python',
+    level: 'Beginner to Advanced',
+    thumbnail: '/images/thumbnails/python.jpg',
+    youtubeId: 'https://youtu.be/_uQrJ0TkZlc?si=__tYcVm5gEk3gx-B',
+    type: 'video'
+  },
+  {
+    id: 9,
+    title: 'UI/UX Design in Figma Full Course',
+    category: 'UI/UX Design',
+    level: 'Beginner',
+    thumbnail: '/images/thumbnails/uiux.jpg',
+    youtubeId: 'https://youtu.be/c9Wg6Cb_YlU?si=UjOso-F81oicG_Bv',
+    type: 'video'
   }
 ]
